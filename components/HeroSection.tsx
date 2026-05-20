6b1f0d4182dd7fbfa9aca80850807e78b09683ed
Eric Liao <125107794+eric1iao@users.noreply.github.com>
2026-05-19 23:57:42 -0400

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
           <Image
-            src="/EricLiao_headshotSQR.jpg"
+            src="/Eric_Headshot_New.jpg"
             alt="Eric Liao Headshot"
             width={325}
             height={325}
